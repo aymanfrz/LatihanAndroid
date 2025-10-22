f5bee19ec4dd2d35c493e9ffa2fd6744f7774425
Ayman <ayman.fahrezi@gmail.com>
2025-10-22 13:45:49 +0700

    Udah ayman edit

--- app/src/androidTest/java/com/ayman/latihan/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/ayman/latihan/ExampleInstrumentedTest.kt
@@ -15,7 +15,7 @@ import org.junit.Assert.*
  */
 @RunWith(AndroidJUnit4::class)
 class ExampleInstrumentedTest {
-    @Test
+    @Test// ini adalah edit ayman
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
