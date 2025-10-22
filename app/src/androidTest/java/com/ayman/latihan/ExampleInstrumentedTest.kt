f2576a50a13dcf418640de9c408301e0dd20718c
Ayman <ayman.fahrezi@gmail.com>
2025-10-22 14:20:31 +0700

    YOU CAN SAVE 50 POUND PER PERSON

--- app/src/androidTest/java/com/ayman/latihan/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/ayman/latihan/ExampleInstrumentedTest.kt
@@ -15,7 +15,7 @@ import org.junit.Assert.*
  */
 @RunWith(AndroidJUnit4::class)
 class ExampleInstrumentedTest {
-    @Test// ini adalah edit Ayman
+    @Test// Editan Ayman
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
